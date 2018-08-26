01a614f9b29b8586fe951601adca320c50d25a9f
Emilian Bold <emilian@emilianbold.ro>
2018-08-26 12:47:32 +0300

    Allows the user to see the date/time in various cities, or the local date/time

--- src/main/java/example/Clock.java
+++ src/main/java/example/Clock.java
@@ -1,27 +1,47 @@
 package example;
 
-import java.util.Date;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Timer;
 import java.util.TimerTask;
+import net.java.html.json.Function;
 import net.java.html.json.Model;
 import net.java.html.json.Property;
 
 @Model(targetId = "", className = "ClockModel", properties = {
     @Property(name = "clock", type = String.class)
+    ,
+    @Property(name = "city", type = String.class)
 })
 public class Clock {
+
+    @Function
+    static void currentLocale(ClockModel model) {
+        model.setCity(ZoneId.systemDefault().getId());
+    }
 
     public static void setUp() {
         ClockModel model = new ClockModel();
         model.setClock("Please wait...");
+        model.setCity("Europe/London");
         model.applyBindings();
         
         Timer timer = new Timer(true);
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                model.setClock(new Date().toString());
+                updateClock(model);
             }
         }, 0, 1000);
+    }
+
+    private static void updateClock(ClockModel model) {
+        Instant instant = Instant.now();
+        ZoneId zoneId = ZoneId.of(model.getCity());
+        ZonedDateTime zdt = ZonedDateTime.ofInstant(instant, zoneId);
+
+        model.setClock(DateTimeFormatter.ofPattern("dd/MM/yyyy - hh:mm:ss").format(zdt));
     }
 }
